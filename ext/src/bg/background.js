f3e3526e7834ecaf452d9bfbf164f780d68e137c
Lipis <lipiridis@gmail.com>
2019-10-09 16:20:06 +0200

    Add Husky and more ESLint rules (#4)
    * Add Husky and more ESLint rules
    
    * Remove

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -5,7 +5,7 @@
 // });
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
   chrome.pageAction.show(sender.tab.id);
   sendResponse();
 });
