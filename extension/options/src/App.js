e32c6c6a296421e439fd899cfe431f5406f8d3e9
Nick McCurdy <nick@nickmccurdy.com>
2019-10-22 09:27:02 -0400

    Persist options with chrome.storage.sync

--- extension/options/src/App.js
+++ extension/options/src/App.js
@@ -1,5 +1,5 @@
-import { Field, Form, Formik } from "formik";
-import React from "react";
+import { Field, Form, Formik, useFormikContext } from "formik";
+import React, { useEffect } from "react";
 
 const defaultOptions = {
   printWidth: 80,
@@ -13,65 +13,83 @@ const defaultOptions = {
   arrowParens: "avoid"
 };
 
-function App() {
-  return (
+function OptionsForm() {
+  const { setValues, values } = useFormikContext();
+
+  useEffect(() => {
+    window.chrome.storage.sync.get(defaultOptions, setValues);
+  }, [setValues]);
+
+  useEffect(() => {
+    if (values) {
+      window.chrome.storage.sync.set(values);
+    }
+  }, [values]);
+
+  return values ? (
     <>
       <h1>
         <a href="https://prettier.io/docs/en/options.html">Prettier Options</a>
       </h1>
-      <Formik initialValues={defaultOptions}>
-        <Form>
-          <hr />
-          <label>Print width</label>
-          <Field type="number" name="printWidth" />
-          <p>Specify the line length that the printer will wrap on.</p>
-          <hr />
-          <label>Tab width</label>
-          <Field type="number" name="tabWidth" />
-          <p>Specify the number of spaces per indentation-level.</p>
-          <hr />
-          <label>Use tabs</label>
-          <Field type="checkbox" name="useTabs" />
-          <p>Indent lines with tabs instead of spaces.</p>
-          <hr />
-          <label>Semicolons</label>
-          <Field type="checkbox" name="semi" />
-          <p>Print semicolons at the ends of statements.</p>
-          <hr />
-          <label>Use single quotes</label>
-          <Field type="checkbox" name="singleQuote" />
-          <p>
-            Use {"'"}single{"'"} quotes instead of {'"'}double{'"'} quotes.
-          </p>
-          <hr />
-          <label>Trailing commas</label>
-          <Field as="select" name="trailingComma">
-            <option>none</option>
-            <option>es5</option>
-            <option>all</option>
-          </Field>
-          <p>Print trailing commas wherever possible.</p>
-          <hr />
-          <label>Bracket spacing</label>
-          <Field type="checkbox" name="bracketSpacing" />
-          <p>Print spaces between brackets in object literals.</p>
-          <hr />
-          <label>JSX Brackets</label>
-          <Field type="checkbox" name="jsxBracketSameLine" />
-          <p>
-            Put the `{">"}` of a multi-line JSX element at the end of the last
-            line instead of being alone on the next line.
-          </p>
-          <hr />
-          <label>Arrow Function Parentheses</label>
-          <Field as="select" name="arrowParens">
-            <option>avoid</option>
-            <option>always</option>
-          </Field>
-          <p>Include parentheses around a sole arrow function parameter.</p>
-        </Form>
-      </Formik>
+      <Form>
+        <hr />
+        <label>Print width</label>
+        <Field type="number" name="printWidth" />
+        <p>Specify the line length that the printer will wrap on.</p>
+        <hr />
+        <label>Tab width</label>
+        <Field type="number" name="tabWidth" />
+        <p>Specify the number of spaces per indentation-level.</p>
+        <hr />
+        <label>Use tabs</label>
+        <Field type="checkbox" name="useTabs" />
+        <p>Indent lines with tabs instead of spaces.</p>
+        <hr />
+        <label>Semicolons</label>
+        <Field type="checkbox" name="semi" />
+        <p>Print semicolons at the ends of statements.</p>
+        <hr />
+        <label>Use single quotes</label>
+        <Field type="checkbox" name="singleQuote" />
+        <p>
+          Use {"'"}single{"'"} quotes instead of {'"'}double{'"'} quotes.
+        </p>
+        <hr />
+        <label>Trailing commas</label>
+        <Field as="select" name="trailingComma">
+          <option>none</option>
+          <option>es5</option>
+          <option>all</option>
+        </Field>
+        <p>Print trailing commas wherever possible.</p>
+        <hr />
+        <label>Bracket spacing</label>
+        <Field type="checkbox" name="bracketSpacing" />
+        <p>Print spaces between brackets in object literals.</p>
+        <hr />
+        <label>JSX Brackets</label>
+        <Field type="checkbox" name="jsxBracketSameLine" />
+        <p>
+          Put the `{">"}` of a multi-line JSX element at the end of the last
+          line instead of being alone on the next line.
+        </p>
+        <hr />
+        <label>Arrow Function Parentheses</label>
+        <Field as="select" name="arrowParens">
+          <option>avoid</option>
+          <option>always</option>
+        </Field>
+        <p>Include parentheses around a sole arrow function parameter.</p>
+      </Form>
     </>
+  ) : null;
+}
+
+function App() {
+  return (
+    <Formik>
+      <OptionsForm />
+    </Formik>
   );
 }
 
