9708cbc397d92314fa9cad02801050f7f307e9aa
Nick McCurdy <nick@nickmccurdy.com>
2019-10-29 10:58:33 -0400

    Improve development with webpack (#59)
    * Hide require.extensions warning
    
    We should revert this after upgrading to Prettier 1.19.
    
    * Make installs faster by not building automatically
    
    * Remove extra whitespace in script
    
    * Only minimize code in production for performance

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin"); // included as a dependency of webpack
 
-module.exports = {
+module.exports = (env, argv) => ({
   entry: "./extension/src/content/index.js",
-  mode: "production",
   optimization: {
-    minimize: true,
     minimizer: [
       new TerserPlugin({
         terserOptions: {
@@ -23,5 +21,11 @@ module.exports = {
   },
   performance: {
     hints: false
-  }
-};
+  },
+  stats: {
+    warningsFilter:
+      // Remove after upgrading to Prettier 1.19
+      "require.extensions is not supported by webpack. Use a loader instead."
+  },
+  watch: argv.mode === "development"
+});
