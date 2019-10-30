b3a22f1706409c05b896ff499686b6e3fea85134
Nick McCurdy <nick@nickmccurdy.com>
2019-10-30 05:24:39 -0400

    Add integration tests for buttons with Jest

---
+++ src/content/index.test.js
@@ -0,0 +1,60 @@
+import "@testing-library/jest-dom/extend-expect";
+import GitHub from "./github";
+import StackOverflow from "./stackOverflow";
+import Storage from "./storage";
+
+window.chrome = {
+  runtime: {},
+  storage: {
+    onChanged: {
+      addListener() {}
+    },
+    sync: {
+      get(callback) {
+        setTimeout(() => callback({}));
+      }
+    }
+  }
+};
+
+window.MutationObserver = class {
+  constructor() {}
+  observe() {}
+};
+
+describe("Prettier format button injection", () => {
+  function createStorage() {
+    const storage = new Storage();
+    return storage.init();
+  }
+
+  function expectToHavePrettierButton() {
+    expect(document.querySelector(".prettier-btn")).toHaveTextContent(
+      "Prettier"
+    );
+  }
+
+  beforeEach(() => (document.body.innerHTML = ""));
+
+  test("GitHub", async () => {
+    // Basis: https://github.com/prettier/prettier-chrome-extension/issues/new
+    const button = document.createElement("button");
+    button.innerText = "Comment";
+    document.body.appendChild(button);
+    // Hack around JSDOM's lack of offsetHeight support to fix isElementVisible
+    Object.defineProperty(document.body, "offsetHeight", { value: 1 });
+
+    new GitHub(await createStorage());
+    expectToHavePrettierButton();
+  });
+
+  test("Stack Overflow", async () => {
+    // Basis: https://stackoverflow.com/questions/51875054
+    const button = document.createElement("div");
+    button.className = "wmd-button-row";
+    document.body.appendChild(button);
+
+    new StackOverflow(await createStorage());
+    expectToHavePrettierButton();
+  });
+});
