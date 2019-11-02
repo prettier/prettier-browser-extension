e0969a34d2f2de56eb63356962654800a51ff348
Nick McCurdy <nick@nickmccurdy.com>
2019-11-01 23:51:14 -0400

    Connect labels to form inputs properly (#65)

--- extension/src/options/index.js
+++ extension/src/options/index.js
@@ -58,95 +58,113 @@ function App() {
         <a href="https://prettier.io/docs/en/options.html">Prettier Options</a>
       </h1>
       <hr />
-      <label>Print width</label>
-      <input
-        type="number"
-        name="printWidth"
-        value={options.printWidth}
-        onChange={handleChange}
-      />
+      <label>
+        Print width
+        <input
+          type="number"
+          name="printWidth"
+          value={options.printWidth}
+          onChange={handleChange}
+        />
+      </label>
       <p>Specify the line length that the printer will wrap on.</p>
       <hr />
-      <label>Tab width</label>
-      <input
-        type="number"
-        name="tabWidth"
-        value={options.tabWidth}
-        onChange={handleChange}
-      />
+      <label>
+        Tab width
+        <input
+          type="number"
+          name="tabWidth"
+          value={options.tabWidth}
+          onChange={handleChange}
+        />
+      </label>
       <p>Specify the number of spaces per indentation-level.</p>
       <hr />
-      <label>Use tabs</label>
-      <input
-        type="checkbox"
-        name="useTabs"
-        checked={options.useTabs}
-        onChange={handleChange}
-      />
+      <label>
+        Use tabs
+        <input
+          type="checkbox"
+          name="useTabs"
+          checked={options.useTabs}
+          onChange={handleChange}
+        />
+      </label>
       <p>Indent lines with tabs instead of spaces.</p>
       <hr />
-      <label>Semicolons</label>
-      <input
-        type="checkbox"
-        name="semi"
-        checked={options.semi}
-        onChange={handleChange}
-      />
+      <label>
+        Semicolons
+        <input
+          type="checkbox"
+          name="semi"
+          checked={options.semi}
+          onChange={handleChange}
+        />
+      </label>
       <p>Print semicolons at the ends of statements.</p>
       <hr />
-      <label>Use single quotes</label>
-      <input
-        type="checkbox"
-        name="singleQuote"
-        checked={options.singleQuote}
-        onChange={handleChange}
-      />
+      <label>
+        Use single quotes
+        <input
+          type="checkbox"
+          name="singleQuote"
+          checked={options.singleQuote}
+          onChange={handleChange}
+        />
+      </label>
       <p>
         Use {"'"}single{"'"} quotes instead of {'"'}double{'"'} quotes.
       </p>
       <hr />
-      <label>Trailing commas</label>
-      <select
-        name="trailingComma"
-        value={options.trailingComma}
-        onChange={handleChange}
-      >
-        <option>none</option>
-        <option>es5</option>
-        <option>all</option>
-      </select>
+      <label>
+        Trailing commas
+        <select
+          name="trailingComma"
+          value={options.trailingComma}
+          onChange={handleChange}
+        >
+          <option>none</option>
+          <option>es5</option>
+          <option>all</option>
+        </select>
+      </label>
       <p>Print trailing commas wherever possible.</p>
       <hr />
-      <label>Bracket spacing</label>
-      <input
-        type="checkbox"
-        name="bracketSpacing"
-        checked={options.bracketSpacing}
-        onChange={handleChange}
-      />
+      <label>
+        Bracket spacing
+        <input
+          type="checkbox"
+          name="bracketSpacing"
+          checked={options.bracketSpacing}
+          onChange={handleChange}
+        />
+      </label>
       <p>Print spaces between brackets in object literals.</p>
       <hr />
-      <label>JSX Brackets</label>
-      <input
-        type="checkbox"
-        name="jsxBracketSameLine"
-        checked={options.jsxBracketSameLine}
-        onChange={handleChange}
-      />
+      <label>
+        JSX Brackets
+        <input
+          type="checkbox"
+          name="jsxBracketSameLine"
+          checked={options.jsxBracketSameLine}
+          onChange={handleChange}
+        />
+      </label>
       <p>
         Put the `{">"}` of a multi-line JSX element at the end of the last line
         instead of being alone on the next line.
       </p>
       <hr />
-      <label>Arrow Function Parentheses</label>
-      <select
-        name="arrowParens"
-        value={options.arrowParens}
-        onChange={handleChange}
-      >
-        <option>avoid</option>
-        <option>always</option>
-      </select>
+      <label>
+        Arrow Function Parentheses
+        <select
+          name="arrowParens"
+          value={options.arrowParens}
+          onChange={handleChange}
+        >
+          <option>avoid</option>
+          <option>always</option>
+        </select>
+      </label>
       <p>Include parentheses around a sole arrow function parameter.</p>
     </>
   ) : null;
