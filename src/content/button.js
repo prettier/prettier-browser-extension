21ebbab6feabbbfad3c7718c93611b2e359d026e
Kai Cataldo <7041728+kaicataldo@users.noreply.github.com>
2019-11-03 01:36:49 -0500

    Fix Create pull request button position (#76)

--- src/content/button.js
+++ src/content/button.js
@@ -1,9 +1,8 @@
-const COMMENT = "Comment";
-const REPLY = "Reply…";
 const CANCEL = "Cancel";
 const CLOSE_ISSUE = " Close issue";
 const CLOSE_PULL_REQUEST = " Close pull request";
-const SUBMIT_PULL_REQUEST = "Create pull request";
+const COMMENT = "Comment";
+const CREATE_PULL_REQUEST = "Create pull request";
 const SUBMIT_NEW_ISSUE = "Submit new issue";
 
 export const BUTTONS = {
@@ -11,17 +10,16 @@ export const BUTTONS = {
   CLOSE_ISSUE,
   CLOSE_PULL_REQUEST,
   COMMENT,
-  REPLY,
-  SUBMIT_NEW_ISSUE,
-  SUBMIT_PULL_REQUEST
+  CREATE_PULL_REQUEST,
+  SUBMIT_NEW_ISSUE
 };
 
 export const BUTTONS_TO_SEARCH_FOR = [
-  COMMENT,
   CANCEL,
   CLOSE_ISSUE,
   CLOSE_PULL_REQUEST,
-  SUBMIT_PULL_REQUEST,
+  COMMENT,
+  CREATE_PULL_REQUEST,
   SUBMIT_NEW_ISSUE
 ];
 
