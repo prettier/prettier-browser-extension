665ef1e362b8c607e2e18f3ee0e77bf2df09b030
Kai Cataldo <7041728+kaicataldo@users.noreply.github.com>
2019-11-04 07:35:35 -0500

    GitHub: add support for Reopen pull request/issue buttons (#79)
    * GitHub: add support for Reopen pull request button
    
    * Add support for Reopen issue button
    
    * Refactor

--- src/content/button.js
+++ src/content/button.js
@@ -3,6 +3,8 @@ const CLOSE_ISSUE = " Close issue";
 const CLOSE_PULL_REQUEST = " Close pull request";
 const COMMENT = "Comment";
 const CREATE_PULL_REQUEST = "Create pull request";
+const REOPEN_ISSUE = "Reopen issue";
+const REOPEN_PULL_REQUEST = "Reopen pull request";
 const SUBMIT_NEW_ISSUE = "Submit new issue";
 
 export const BUTTONS = {
@@ -11,6 +13,8 @@ export const BUTTONS = {
   CLOSE_PULL_REQUEST,
   COMMENT,
   CREATE_PULL_REQUEST,
+  REOPEN_ISSUE,
+  REOPEN_PULL_REQUEST,
   SUBMIT_NEW_ISSUE
 };
 
@@ -20,7 +24,19 @@ export const BUTTONS_TO_SEARCH_FOR = [
   CLOSE_PULL_REQUEST,
   COMMENT,
   CREATE_PULL_REQUEST,
+  REOPEN_ISSUE,
+  REOPEN_PULL_REQUEST,
   SUBMIT_NEW_ISSUE
+];
+
+const CLOSE_PR_ISSUE_BUTTON = "button[name=comment_and_close]";
+const REOPEN_PR_ISSUE_BUTTON = "button[name=comment_and_open]";
+const CANCEL_COMMENT_BUTTON = "button[name=comment_and_open]";
+
+export const COMMENT_SIBLING_SELECTORS_TO_DEFER_TO = [
+  CLOSE_PR_ISSUE_BUTTON,
+  REOPEN_PR_ISSUE_BUTTON,
+  CANCEL_COMMENT_BUTTON
 ];
 
 export default function renderButton(
