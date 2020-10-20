c414eb0f498ba5a7bdd05b889c0e72dbd3203c4c
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2020-10-20 11:50:26 +0530

    Webpack 5 + New JSX Transform (#363)
    * Update to webpack 5
    
    * Update Babel+React
    
    * Use new jsx transform
    
    * Disable eslint rule react-in-jsx-scope

--- src/options/index.js
+++ src/options/index.js
@@ -1,7 +1,6 @@
 import "./index.scss";
 
 import AppContainer from "./AppContainer";
-import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(<AppContainer />, document.getElementById("root"));
